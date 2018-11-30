17b5da22c06b8f640e5937611c34c16832465d34
Sreeram Vasudevan <sreeram.vasudevan@gmail.com>
2018-12-01 00:36:24 +0800

    INITIAL Commit: client integration and server integration

---
+++ client_integration/README.md
@@ -0,0 +1,13 @@
+#Paypal Button Client Integration
+
+## Description
+This is a client integration implementation of the paypal button. A sample integration where a user buys a book through paypal
+
+## Pre-requisites
+- To be run in a browser that has no url block
+- The running system needs to have internet access
+
+## Running the code
+- Run the `checkout.html` in the browser
+- Use the buyer credentials to buy the book - **sreeram.vasudevan-buyer@gmail.com**
+- Try a checkout with Paypal.
---
+++ server_integration/server.js
@@ -0,0 +1,112 @@
+// Server implementation
+
+var express = require('express');
+var request = require('request');
+var path = require('path');
+
+var CLIENT = 'AaNponrDq6jygBDh5pf6x_TIpqdLp_06juKN8TQtNrgLPMvvjJ-OQMYaqtaVA_Y6SquR1jW4V52MoUr5';
+var SECRET = 'EFgYuGqmJlcL9lYjLUYYJs3kZdz70eUv96KExzZJjUHzCSgdLvPyyV1AnU0U_1kUubXqVIzknBMK-xm4';
+var PAYPAL_API = 'https://api.sandbox.paypal.com';
+
+var app = express();
+
+  // Set up the payment:
+  // 1. Set up a URL to handle requests from the PayPal button
+  app.post('/my-api/create-payment/', function(req, res)
+  {
+    // 2. Call /v1/payments/payment to set up the payment
+    request.post(PAYPAL_API + '/v1/payments/payment',
+    {
+      auth:
+      {
+        user: CLIENT,
+        pass: SECRET
+      },
+      body:
+      {
+        intent: 'sale',
+        payer:
+        {
+          payment_method: 'paypal'
+        },
+        transactions: [
+        {
+          amount:
+          {
+            total: '0.01',
+            currency: 'USD'
+          }
+        }],
+        redirect_urls:
+        {
+          return_url: 'http://localhost:3000/my-api/execute-payment',
+          cancel_url: 'http://localhost:3000'
+        }
+      },
+      json: true
+    }, function(err, response)
+    {
+      if (err)
+      {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+      // 3. Return the payment ID to the client
+      res.json(
+      {
+        id: response.body.id
+      });
+    });
+  })
+  // Execute the payment:
+  // 1. Set up a URL to handle requests from the PayPal button.
+  .post('/my-api/execute-payment/', function(req, res)
+  {
+    // 2. Get the payment ID and the payer ID from the request body.
+    var paymentID = req.body.paymentID;
+    var payerID = req.body.payerID;
+    // 3. Call /v1/payments/payment/PAY-XXX/execute to finalize the payment.
+    request.post(PAYPAL_API + '/v1/payments/payment/' + paymentID +
+      '/execute',
+      {
+        auth:
+        {
+          user: CLIENT,
+          pass: SECRET
+        },
+        body:
+        {
+          payer_id: payerID,
+          transactions: [
+          {
+            amount:
+            {
+              total: '0.01',
+              currency: 'USD'
+            }
+          }]
+        },
+        json: true
+      },
+      function(err, response)
+      {
+        if (err)
+        {
+          console.error(err);
+          return res.sendStatus(500);
+        }
+        // 4. Return a success response to the client
+        res.json(
+        {
+          status: 'success'
+        });
+      });
+  })
+  // Displaying the checkout page at the start
+  .get('/', (req,res) => {
+    res.sendFile(path.join(__dirname+'/checkout.html'));
+  }).listen(3000, function()
+  {
+    console.log('Server listening at http://localhost:3000/');
+  });
+// Run `node ./server.js` in your terminal
